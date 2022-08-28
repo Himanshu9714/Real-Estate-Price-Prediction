fc619b0da1f509c8152ec81720f0710dab6fb140
Himanshu Suthar <h9714348433@gmail.com>
2022-08-28 22:15:32 +0530

    Create endpoints for price prediction and listing locations

---
+++ server/util.py
@@ -0,0 +1,64 @@
+import pickle
+import json
+from pathlib import Path
+import os
+import numpy as np
+
+CURRENT_DIR = Path(__file__).resolve().parent
+
+# Global variables
+__locations = None
+__data_columns = None
+__model = None
+
+
+def get_estimated_price(location, sqft, bhk, bath):
+    """Return price for given feature"""
+    try:
+        # Find index for specific location
+        loc_index = __data_columns.index(location).lower()
+    except:
+        # If given location doesn't found in the list
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    # predict(): Takes an input X, and for that given input it returns the output.
+    # In our example, it will return estimated price.
+    return round(__model.predict([x])[0], 2)
+
+
+def get_location_names():
+    return __locations
+
+
+def load_saved_artifacts():
+    print("Loading saved artifacts...start")
+    global __data_columns
+    global __locations
+    global __model
+
+    if not __data_columns or not __locations or not __model:
+        # Load columns from the columns.json and returns all the columns starting from index 3,
+        # as first three columns are not locations columns
+        with open(os.path.join(CURRENT_DIR, "artifacts/columns.json"), "r") as f:
+            __data_columns = json.load(f)["data_columns"]
+            __locations = __data_columns[3:]
+
+        # Load the model we got from the data cleaning
+        with open(
+            os.path.join(CURRENT_DIR, "artifacts/banglore_home_prices_model.pickle"),
+            "rb",
+        ) as f:
+            __model = pickle.load(f)
+
+        print("Including saved artifacts...done!")
+
+
+load_saved_artifacts()
